3dd5d720f351462106fea31f7346ffc5b64f5c1e
NivasNanthini <nivas.srinins1995@gmail.com>
2024-12-17 16:27:43 +0530

    Sri's New Code

--- src/test/java/org/steps/StepsDefinition.java
+++ src/test/java/org/steps/StepsDefinition.java
@@ -5,6 +5,8 @@ public class StepsDefinition {
 	public static void main(String[] args) {
 		
 		System.out.println("Test 1");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
 		System.out.println("Test 2");
 		System.out.println("Test 3");
 		
